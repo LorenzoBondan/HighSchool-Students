4cf57093bc616dafaf3ae3444c126bf92cf7917b
LorenzoBondan <lbbondan@gmail.com>
2023-02-06 11:56:40 -0300

    StudentCard, courses

--- frontend/src/components/StudentCard/index.tsx
+++ frontend/src/components/StudentCard/index.tsx
@@ -8,8 +8,6 @@ type Props = {
 
 function StudentCard( {student} : Props ) {
 
-  //const [page, setPage] = useState<Student>();
-
     return (
       <>
         <div className='base-card product-card'>
@@ -18,15 +16,20 @@ function StudentCard( {student} : Props ) {
             </div>
 
             <div className='card-bottom-container'>
+              <>
                 <h3>{student.nickname}</h3>
                 <h6>Name: {student.name}</h6>
                 <h6>Age: {student.age}</h6>
 
                 {student.graduated ? <h6>Graduated: true</h6> : <h6>Graduated: false</h6>}
-
-                <h6>Courses: </h6>
+                
+                <div className='courses-zone'>
+                  <h6>Courses: </h6>
+                  {student.courses.map(course => (<h5>{course.name}</h5>))}
+                </div>
                 
 
+                </>
             </div>
         </div>
         

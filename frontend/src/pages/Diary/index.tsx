b11c703ba6f8143138e25c36ba67a7508e0c1d73
LorenzoBondan <lbbondan@gmail.com>
2023-02-14 16:03:50 -0300

    Diary page

--- frontend/src/pages/Diary/index.tsx
+++ frontend/src/pages/Diary/index.tsx
@@ -5,16 +5,18 @@ import './styles.css';
 const Diary = () => {
     return(
        <div className='diary-page-container'>
-            <div className='diary-page-buttons-container'>
+            <div className='base-card diary-page-buttons-container'>
                 <h1 className='text-primary'>Select the year</h1>
 
+                <Link to='/diary/2016'>
+                    <button className='btn btn-primary'>2016</button>
+                </Link>
+                
                 <Link to='/diary/2015'>
                     <button className='btn btn-primary'>2015</button>
                 </Link>
                 
-                <Link to='/diary/2016'>
-                    <button className='btn btn-primary'>2016</button>
-                </Link>
+                
             </div>
        </div>
     );

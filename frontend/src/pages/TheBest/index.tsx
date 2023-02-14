faf26d01a0f90997341b9082068ab276f1d95f3a
LorenzoBondan <lbbondan@gmail.com>
2023-02-14 20:48:22 -0300

    Moments page html

--- frontend/src/pages/TheBest/index.tsx
+++ frontend/src/pages/TheBest/index.tsx
@@ -1,9 +1,46 @@
 
+import { AxiosRequestConfig } from 'axios';
+import TheBestCard from 'components/TheBestCard';
+import { useEffect, useState } from 'react';
+import { Moment } from 'types/Moment';
+import { SpringPage } from 'types/vendor/spring';
+import { requestBackend } from 'util/requests';
 import './styles.css';
 
 const TheBest = () => {
+
+    const [page, setPage] = useState<SpringPage<Moment>>();
+
+    useEffect(() => {
+        const params : AxiosRequestConfig = {
+            url: '/moments',
+            params: {
+              page: 0,
+              size: 50,
+            },
+          };
+      
+          requestBackend(params).then((response) => {
+            setPage(response.data);
+          });
+      }, []);
+
     return(
-        <h1>the best</h1>
+        <div className='the-best-page-container'>
+            <div className='the-best-page-top-container'>
+                <h1>The best moments of the year</h1>
+            </div>
+
+            <div className='the-best-page-content-container'>
+                {page?.content.map((moment) => (
+                    <div>
+                        <TheBestCard moment={moment}/>
+                    </div>
+                ))}
+            </div>
+            
+        </div>
+
     );
 }
 

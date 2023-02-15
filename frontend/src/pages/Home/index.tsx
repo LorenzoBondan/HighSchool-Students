7e97e2409657bd0ddd802b0ac03e80641fab8e7c
LorenzoBondan <lbbondan@gmail.com>
2023-02-15 10:58:28 -0300

    Home button icons

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,8 +4,14 @@ import background from 'assets/images/turma.jpg';
 import logoTerceirao from 'assets/images/logo-terceirao.png';
 import logoSagrado from 'assets/images/logo-sagrado.png';
 
+import { FaUsers, FaTrophy } from 'react-icons/fa';
+import { BsFillJournalBookmarkFill  } from 'react-icons/bs';
+import { TfiAlarmClock } from 'react-icons/tfi';
+import { MdAdminPanelSettings } from 'react-icons/md';
+
 function Home() {
     return (
+        
         <>
             <div className="base-card main-container"  style={{backgroundImage: `url(${background})`,
                                                             backgroundRepeat: "no-repeat",
@@ -17,23 +23,58 @@ function Home() {
                     <p>A summary of the class of 1999</p>
 
                     <Link to="/students">
-                        <button className='btn btn-primary'>Students</button>
+                        <button className='btn btn-primary'>
+                            <i style={{
+                                marginRight:"8px",
+                                }}>
+                                    <FaUsers/>
+                            </i>
+                            Students
+                        </button>
+                        
                     </Link>
 
                     <Link to="/thebest">
-                        <button className='btn btn-primary'>The best of the year</button>
+                        <button className='btn btn-primary'>
+                        <i style={{
+                                marginRight:"8px",
+                                }}>
+                                    <FaTrophy/>
+                            </i>
+                            The best of the year</button>
                     </Link>
 
                     <Link to="/diary">
-                        <button className='btn btn-primary'>The author's diary</button>
+                        <button className='btn btn-primary'>
+                            <i style={{
+                                marginRight:"8px",
+                                }}>
+                                    <BsFillJournalBookmarkFill/>
+                            </i>
+                            The author's diary
+                        </button>
                     </Link>
 
                     <Link to="/countdown">
-                        <button className='btn btn-primary'>Countdown to Isabel's party</button>
+                        <button className='btn btn-primary'>
+                            <i style={{
+                                marginRight:"8px",
+                                }}>
+                                    <TfiAlarmClock/>
+                            </i>
+                            Countdown to Isabel's party
+                        </button>
                     </Link>
 
                     <Link to="/admin">
-                        <button className='btn btn-primary'>ADMIN</button>
+                        <button className='btn btn-primary'>
+                            <i style={{
+                                marginRight:"8px",
+                                }}>
+                                    <MdAdminPanelSettings/>
+                            </i>
+                            ADMIN
+                        </button>
                     </Link>
 
 
